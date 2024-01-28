e5a0abfc6735fbb7adaadc302b769164cdba0512
Louktarn <aunnicha268@gmail.com>
2024-01-28 23:47:06 +0700

    first commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const mockEmployees = [
   {
     id: 0,
@@ -17,17 +19,21 @@ const mockEmployees = [
     lastname: "lord",
     position: "Designer"
   },
-]
+];
 
 const Home = () => {
-
   return (
     <div>
-
+      <h2>Employee List</h2>
+      <ul>
+        {mockEmployees.map(employee => (
+          <li key={employee.id}>
+            <strong>{`${employee.name} ${employee.lastname}`}</strong> - {employee.position}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-
-
-export default Home
+export default Home;
